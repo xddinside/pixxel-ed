54a5e8294a587494aa9f370ccf6edf2763dbe68c
Nirvaan Salian <nirvaansaalian@gmail.com>
2025-08-08 11:41:16 +0530

    Navbar with theme toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { Button } from "./ui/button"; // shadcn Button
-import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./ui/ModeToggle";
 
-import { UserButton, useUser } from "@clerk/nextjs";
-import { ChefHat } from "lucide-react";
+import { BookOpenText } from "lucide-react";
 
 export default function Navbar() {
-  const { isSignedIn } = useUser();
-
-  // We no longer need useClerk or openSignIn/openSignUp
-  // const { openSignIn, openSignUp } = useClerk();
 
   return (
     <nav className="border-b border-border py-5 flex items-center justify-between ">
       <div className="flex items-center gap-6 px-4 sm:px-6">
         <h1 className="text-3xl font-semibold">
           <Link href={"/"} className="flex gap-2">
-            <ChefHat className="size-8" />
-            <span><span className="text-amber-500">Flavor</span>Flow</span>
+            <BookOpenText className="size-8" />
+            <span>Pixxel<span className="text-blue-500">Ed</span></span>
           </Link>
         </h1>
       </div>
@@ -27,18 +22,14 @@ export default function Navbar() {
       <div className="flex items-center gap-4 px-4 sm:px-6">
         <ModeToggle />
 
-        {!isSignedIn ? (
-          <div className="hidden md:flex items-center gap-4">
-            <Button asChild>
-              <Link href="/login">Login</Link>
-            </Button>
-            <Button variant={"ghost"} asChild>
-              <Link href="/sign-up">Sign Up</Link>
-            </Button>
-          </div>
-        ) : (
-          <UserButton afterSignOutUrl="/" />
-        )}
+        <div className="hidden md:flex items-center gap-4">
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button variant={"ghost"} asChild>
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </div>
       </div>
     </nav>
   );
